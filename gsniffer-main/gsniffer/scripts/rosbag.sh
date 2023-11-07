00095128df07fc32ea8757a72b5daa8eb710a765
ecunhado <eddcmac1999@gmail.com>
2023-11-07 15:12:44 +0000

    remove gsniffer driver

--- gsniffer-main/gsniffer/scripts/rosbag.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-source /home/pi/gsniffer_ws/devel/setup.bash
-
-BAGS_FOLDER='/media/data/bags'
-mkdir -p ${BAGS_FOLDER}
-cd ${BAGS_FOLDER}
-
-rosbag record -a --split --size 1024 --lz4
